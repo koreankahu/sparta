fe3af65e1e00d07f20c02db22f0b565c9f02699c
koreankahu <65495825+koreankahu@users.noreply.github.com>
2020-07-02 23:03:16 +0900

    새가날아든다

---
+++ projects/birdcomming/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+
+# URL 별로 함수명이 같거나,
+# route('/') 등의 주소가 같으면 안됩니다.
+client = MongoClient('localhost', 27017)
+db = client.bird_comming
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+@app.route('/message', methods=["POST"])
+def set_message():
+    username_receive = request.form['username_give']
+    contents_receive = request.form['contents_give']
+
+    doc = {
+        'username': username_receive,
+        'contents': contents_receive,
+        'created_at': datetime.now()
+    }
+
+    db.messages.insert_one(doc)
+
+    return jsonify({'result': 'success', 'msg': '메시지 작성에 성공하였습니다!'})
+
+@app.route('/message', methods=["GET"])
+def get_message():
+    #지금 시간 구하기
+    date_now=datetime.now()
+    #하루전시간 구하기
+    date_before = date_now - timedelta(days=1)    
+    messages = list(db.messages.find({'created_at':{'$gte':date_before, '$lte':date_now}}, {'_id':0}).sort('created_at', -1))
+    return jsonify({'result': 'success', 'messages':messages})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
